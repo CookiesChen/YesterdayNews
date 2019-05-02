f9f20850ed3fbe29e03895d6ff9af23b0b2f4997
CookiesChen <1109349604@qq.com>
2019-05-03 00:50:32 +0800

    Revert "Personal Page"

--- YesterdayNews/ViewModel/UserInfo/UserInfoViewModel.h
+++ YesterdayNews/ViewModel/UserInfo/UserInfoViewModel.h
@@ -6,22 +6,7 @@
 //  Copyright © 2019 Cookieschen. All rights reserved.
 //
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import "ReactiveObjC.h"
-
-typedef NS_ENUM(NSInteger,OperationType) {
-    LOGIN = 0,
-    SIGNUP = 1,
-};
-
 
 @interface UserInfoViewModel: NSObject
-
-// 显示登录还是注册页面
-@property (assign, nonatomic) OperationType operationType;
-// 登录页面透明度
-@property (assign, nonatomic) BOOL hideLoginView;
-// 注册页面透明度
-@property (assign, nonatomic) BOOL hideSignupView;
 
 @end
