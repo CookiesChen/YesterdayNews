e1e396e62b4ddc5c47acc165a89916349015a814
Cookieschen <1109349604@qq.com>
2019-05-11 11:49:25 +0800

    fix size

---
+++ YesterdayNews/Layout/NewsLayout.h
@@ -0,0 +1,13 @@
+//
+//  NewsLayout.h
+//  YesterdayNews
+//
+//  Created by Cookieschen on 2019/5/11.
+//  Copyright © 2019 Cookieschen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsLayout : UICollectionViewFlowLayout
+
+@end
