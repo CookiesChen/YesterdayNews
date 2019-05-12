a8b4606aecff5d58feeec1d17f87e65f3439dda1
陈冬禹 <cdy@chendongyudeMac.local>
2019-05-13 01:06:00 +0800

    update newsdetail

--- YesterdayNews/ViewController/NewsDetail/NewsDetailViewController.m
+++ YesterdayNews/ViewController/NewsDetail/NewsDetailViewController.m
@@ -10,15 +10,23 @@
 #import <Colours.h>
 #import <ReactiveObjC.h>
 
+#define StatusBarHeight [[UIApplication sharedApplication] statusBarFrame].size.height
+#define NavBarHeight 44
+#define NavHeight (StatusBarHeight + NavBarHeight)
+#define ScreenWidth [UIScreen mainScreen].bounds.size.width
+#define ScreenHeight [UIScreen mainScreen].bounds.size.height
+
+
 
 @interface NewsDetailViewController()
 
-@property(nonatomic, strong) UIToolbar *topBar;
+@property(nonatomic, strong) UIView *topBar;
 @property(nonatomic, strong) UIButton *backButton;
 @property(nonatomic, strong) UIImage *topImg;
 @property(nonatomic, strong) UIButton *searchButton;
 @property(nonatomic, strong) UIButton *moreButton;
-@property(nonatomic, strong) NSMutableArray<UIBarButtonItem*> *itemArr;
+
+- (void)addBackBtn;
 
 @end
 
@@ -51,10 +59,10 @@
 - (void)setupView {
     [self.view setFrame:[UIScreen mainScreen ].bounds];
     [self.view setBackgroundColor: [UIColor whiteColor]];
-    [self.view addSubview:self.topBar];
-    [self.view addSubview:self.backButton];
-    [self.view addSubview:self.searchButton];
-    [self.view addSubview:self.moreButton];
+    _topBar = [[UIView alloc] initWithFrame:CGRectMake(0, StatusBarHeight, ScreenWidth, ScreenHeight)];
+    _topBar.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:_topBar];
+    [self addBackBtn];
 }
 
 // viewmodel绑定
@@ -62,36 +70,16 @@
     
 }
 
-- (UIButton *)backButton {
-    if(_backButton == nil) {
-        _backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 50, 30, 30)];
-        [_backButton setTitle:@"<" forState:UIControlStateNormal];
-        _backButton.titleLabel.font = [UIFont systemFontOfSize:20];
-        NSMutableAttributedString *str0 = [[NSMutableAttributedString alloc] initWithString:_backButton.titleLabel.text];
-        [str0 addAttribute:(NSString*)NSForegroundColorAttributeName value:[UIColor blackColor] range:[_backButton.titleLabel.text rangeOfString:((UIButton *)_backButton).titleLabel.text]];
-        [_backButton setAttributedTitle:str0 forState:UIControlStateNormal];
-        _backButton.titleLabel.lineBreakMode = NSLineBreakByCharWrapping;
-        [[_backButton rac_signalForControlEvents: UIControlEventTouchUpInside] subscribeNext:^(UIButton *x) {
-            [self.navigationController popViewControllerAnimated:YES];
-        }];
-    }
-    return _backButton;
-}
-
-- (UIToolbar *)topBar {
-    if(_topBar == nil) {
-        [self.navigationController setToolbarHidden:NO animated:YES];
-        _topBar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 50, 600, 30)];
-        UIBarButtonItem *backItem = [[UIBarButtonItem alloc] initWithCustomView:_backButton];
-        UIBarButtonItem *searchItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSearch target:self action:nil];
-        _itemArr = [[NSMutableArray alloc]initWithObjects:backItem, searchItem, nil];
-        [_topBar setItems:_itemArr];
-    }
-    return _topBar;
+// 添加返回按钮
+- (void)addBackBtn {
+    UIImage *backgroundImg = [UIImage imageNamed:@"9"];
+    _backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [_backButton setBackgroundImage:backgroundImg forState:UIControlStateNormal];
+    _backButton.frame = CGRectMake(0, 0, 60, 44);
+    [_topBar addSubview:_backButton];
 }
 
 /* -- progma mark - UICollectionViewDelegate -- */
-
 
 /* -- progma mark - getters and setters -- */
 
